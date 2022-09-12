16a684bd70f4cbb31c82ce1220ddbf0da3e14cda
Efremov Valentin <Shadow77746@gmail.com>
2022-09-12 23:33:03 +0300

    push

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,7 +1,7 @@
 # posts/urls.py
 from django.urls import path
 from posts.views import (group_posts, index, post_create,
-                         edit, post_view, profile, page_not_found,
+                         post_edit, post_view, profile, page_not_found,
                          follow_index, profile_follow, profile_unfollow,
                          add_comment)
 
@@ -11,7 +11,7 @@ urlpatterns = [
     path('profile/<str:username>/', profile, name='profile'),
     path('posts/<int:post_id>/', post_view, name='post_detail'),
     path('create/', post_create, name='post_create'),
-    path('posts/<int:post_id>/edit/', edit, name='edit'),
+    path('posts/<int:post_id>/edit/', post_edit, name='edit'),
     path('follow/', follow_index, name='follow_index'),
     path("404/", page_not_found, name="404"),
     path('', index, name='index'),
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -89,7 +89,7 @@ def post_create(request):
 
 
 @login_required
-def edit(request, post_id):
+def post_edit(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     if post.author != request.user:
         return redirect('posts:post_detail', post_id=post_id)
