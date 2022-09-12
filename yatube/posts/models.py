d8b5c4fcd6598133cea9de97e3c1ef912a551922
Efremov Valentin <Shadow77746@gmail.com>
2022-09-12 15:38:10 +0300

    add new models

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -37,3 +37,53 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.text[:15]}"
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Пост')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор')
+    text = models.TextField(
+        verbose_name='Коментарий')
+    created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Создан')
+    updated = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Обнавлен')
+    active = models.BooleanField(
+        default=True,
+        verbose_name='Активен')
+
+    class Meta:
+        ordering = ['-created']
+        verbose_name_plural = 'Коментарии'
+        verbose_name = 'Коментарий'
+
+    def __str__(self):
+        return self.text[:15]
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Пользователь')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='Автор')
+
+    class Meta:
+        verbose_name_plural = 'Подписки'
+        verbose_name = 'Подписка'
+
+    def __str__(self):
+        return f'{self.user} подписался на {self.author}'
